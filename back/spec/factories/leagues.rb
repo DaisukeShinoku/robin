0519b03953fa73dfc9d5191a9319494cdfb7f63b
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2024-04-13 19:03:23 +0900

    leagueモデルのRSpec追加

---
+++ back/spec/factories/leagues.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :league do
+    name { "テスト用リーグ" }
+  end
+end
---
+++ back/spec/models/league_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe League, type: :model do
+  describe 'validations' do
+    it "nameがない場合は無効であること" do
+      league = build(:league, name: nil)
+      expect(league).to be_invalid
+    end
+
+    it "空文字の場合は無効であること" do
+      league = build(:league, name: "")
+      expect(league).to be_invalid
+    end
+
+    it "nameが50文字以上の場合は無効であること" do
+      league = build(:league, name: "a" * 51)
+      expect(league).to be_invalid
+    end
+
+    it "nameが50文字以下の場合は有効であること" do
+      league = build(:league, name: "a" * 50)
+      expect(league).to be_valid
+    end
+  end
+end
